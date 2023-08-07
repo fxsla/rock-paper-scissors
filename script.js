19bec86d13655d0a9ee87223867e3c525149a4f8
fxsla <fuhadulislam02@gmail.com>
2023-08-07 12:42:28 +0100

    Add rps UI version

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ function getComputerChoice() {
 
 }
 
-function  play(){
-    let playerSelection = prompt("Enter rock, paper or scissors").toLowerCase()
+
+function  play(playerSelect){
+    let playerSelection = playerSelect
     let computerSelection= getComputerChoice()
 
     return parseInt(playRound(playerSelection, computerSelection))
@@ -23,31 +24,85 @@ function playRound(playerSelection, computerSelection){
 
     if(computerSelection === "rock" && playerSelection === "paper") {
 
-        console.log("You win! Paper beats Rock!")
+        createParagraph("You win! Paper beats Rock!");
         return 1;
 
     } else if(computerSelection === "scissors" && playerSelection === "rock"){
 
-        console.log("You win! Rock beats Scissors!")
+    
+        createParagraph("You win! Rock beats Scissors!");
         return 1;
         
     } else if(computerSelection === "paper" && playerSelection === "scissors"){
 
-        console.log("You win! Scissors beats Paper!")
+        createParagraph("You win! Scissors beats Paper!");
+        
         return 1;
 
     } else if(computerSelection === playerSelection) {
         
-        console.log("Draw!")
+        createParagraph("Draw!");
         return 0;
 
     } else {
 
-        console.log(`You lose, ${computerSelection} beats ${playerSelection}`)
-        return 0;
+        createParagraph(`You lose, ${computerSelection} beats ${playerSelection}`)
+        return -1;
     }
 }
 
+function createParagraph(text){
+    let paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    container.before(paragraph)
+}
+
+
+
+let buttons = document.querySelectorAll("button");
+let container = document.querySelector(".results");
+let playerPara = document.createElement("p");
+let computerPara = document.createElement("p");
+playerPara.textContent = "Player Score: ";
+computerPara.textContent = "Computer Score: ";
+container.appendChild(playerPara);
+container.appendChild(computerPara);
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let singleRound = play(button.value);
+        if(singleRound == 1){
+            playerScore++;
+            playerPara.textContent = `Player Score: ${playerScore}`;
+        } else if(singleRound == -1){
+            computerScore ++;
+            computerPara.textContent = `Computer Score: ${computerScore}`;
+        }
+
+        if(playerScore == 5){
+            console.log("player wins")
+            winnerText.textContent = "Player wins!";
+            winnerContainer.appendChild(winnerText);
+        } else if(computerScore == 5) {
+            console.log("computer wins")
+            winnerText.textContent = "Computer wins!";
+            winnerContainer.appendChild(winnerText);
+
+        }
+    })
+})
+
+let winnerContainer = document.querySelector(".winnerContainer")
+let winnerText = document.createElement("p");
+
+
+
+
+
+/*
 function game() {
     let score = 0;
 
@@ -59,3 +114,5 @@ function game() {
 }
 
 game();
+
+*/
